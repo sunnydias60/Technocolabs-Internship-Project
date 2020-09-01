f08b9865fe57e1233e258c95cb6753dade824a87
sunnydias60 <44764583+sunnydias60@users.noreply.github.com>
2020-09-02 02:42:04 +0530

    Add files via upload

---
+++ Project task/task-18/MessageMixer18.js
@@ -0,0 +1,69 @@
+
+
+function countCharacter(inputString, inputCharacter) {
+  let count = 0;
+  let string = inputString.toLowerCase();
+  let character = inputCharacter.toLowerCase();
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === character) {
+         count++;
+      }
+    }
+  return count; 
+};
+
+function capitalizeFirstCharacterOfWords(string) {
+  let arr = string.split(" ");  
+    for (let i = 0; i < arr.length; i++) {  
+      let word = arr[i];
+        arr[i] = word[0].toUpperCase() + word.substring(1); 
+    }
+  return arr.join(" "); 
+};
+
+
+function reverseWord(word) {
+  return word.split("").reverse().join("");
+};
+
+function reverseAllWords(sentence) {
+  let words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = MessageMixer.reverseWord(words[i]);
+    }
+   return words.join(" ");
+};
+
+
+function replaceFirstOccurence(string, toBeReplaced, replaceWith) {
+  return string.replace(toBeReplaced, replaceWith);
+};
+
+
+function replaceAllOccurrences(string, toBeReplaced, replaceWith) {
+  return string.split(toBeReplaced).join(replaceWith);
+};
+
+function encode(string) {
+  let replacementObject = { "a": "@", "s": "$", "i": "!", "o":"0" };
+    for (let key in replacementObject) {
+      string = MessageMixer.replaceAllOccurrences(string, key, replacementObject[key]); 
+    }	
+    return string;
+};
+
+function palindrome(str) {
+    return (`${str} ${MessageMixer.reverseWord()}`);
+}
+
+function pigLatin(sentence, character) {
+    let w = sentence.split(" ");
+    return w.join(character + " ");    
+}
+//displayMessage();
+
+var MessageMixer = new Object();
+
+export { MessageMixer as default };
+
+
